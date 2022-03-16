e1f40db4bcbc3aba321faca7cc5da30343501296
Nikkey <55837837+Nikkey-Liu@users.noreply.github.com>
2022-03-16 14:36:42 +0800

    fix bug
    fix 刷新后数据重复

--- src/LineChartEchart.editorPreview.tsx
+++ src/LineChartEchart.editorPreview.tsx
@@ -1,28 +1,26 @@
-import { Component, ReactNode,createElement } from "react";
+import { Component, ReactNode, createElement } from "react";
 // import { HelloWorldSample } from "./components/HelloWorldSample";
 import { LineChartEchartPreviewProps } from "../typings/LineChartEchartProps";
-import EChartsReact from'echarts-for-react';
+import EChartsReact from "echarts-for-react";
 declare function require(name: string): string;
 
 export class preview extends Component<LineChartEchartPreviewProps> {
-    
-  
     render(): ReactNode {
         const option = {
             title: {
-                text: '堆叠区域图'
+                text: "堆叠区域图"
             },
             tooltip: {
-                trigger: 'axis',
+                trigger: "axis",
                 axisPointer: {
-                    type: 'cross',
+                    type: "cross",
                     label: {
-                        backgroundColor: '#6a7985'
+                        backgroundColor: "#6a7985"
                     }
                 }
             },
             legend: {
-                data: ['邮件营销', '联盟广告', '视频广告', '直接访问', '搜索引擎']
+                data: ["邮件营销", "联盟广告", "视频广告", "直接访问", "搜索引擎"]
             },
             toolbox: {
                 feature: {
@@ -30,85 +28,82 @@ export class preview extends Component<LineChartEchartPreviewProps> {
                 }
             },
             grid: {
-                left: '3%',
-                right: '4%',
-                bottom: '3%',
+                left: "3%",
+                right: "4%",
+                bottom: "3%",
                 containLabel: true
             },
             xAxis: [
                 {
-                    type: 'category',
+                    type: "category",
                     boundaryGap: false,
-                    data: ['周一', '周二', '周三', '周四', '周五', '周六', '周日']
+                    data: ["周一", "周二", "周三", "周四", "周五", "周六", "周日"]
                 }
             ],
             yAxis: [
                 {
-                    type: 'value'
+                    type: "value"
                 }
             ],
             series: [
                 {
-                    name: '邮件营销',
-                    type: 'line',
-                    stack: '总量',
+                    name: "邮件营销",
+                    type: "line",
+                    stack: "总量",
                     areaStyle: {},
                     emphasis: {
-                        focus: 'series'
+                        focus: "series"
                     },
                     data: [120, 132, 101, 134, 90, 230, 210]
                 },
                 {
-                    name: '联盟广告',
-                    type: 'line',
-                    stack: '总量',
+                    name: "联盟广告",
+                    type: "line",
+                    stack: "总量",
                     areaStyle: {},
                     emphasis: {
-                        focus: 'series'
+                        focus: "series"
                     },
                     data: [220, 182, 191, 234, 290, 330, 310]
                 },
                 {
-                    name: '视频广告',
-                    type: 'line',
-                    stack: '总量',
+                    name: "视频广告",
+                    type: "line",
+                    stack: "总量",
                     areaStyle: {},
                     emphasis: {
-                        focus: 'series'
+                        focus: "series"
                     },
                     data: [150, 232, 201, 154, 190, 330, 410]
                 },
                 {
-                    name: '直接访问',
-                    type: 'line',
-                    stack: '总量',
+                    name: "直接访问",
+                    type: "line",
+                    stack: "总量",
                     areaStyle: {},
                     emphasis: {
-                        focus: 'series'
+                        focus: "series"
                     },
                     data: [320, 332, 301, 334, 390, 330, 320]
                 },
                 {
-                    name: '搜索引擎',
-                    type: 'line',
-                    stack: '总量',
+                    name: "搜索引擎",
+                    type: "line",
+                    stack: "总量",
                     label: {
                         show: true,
-                        position: 'top'
+                        position: "top"
                     },
                     areaStyle: {},
                     emphasis: {
-                        focus: 'series'
+                        focus: "series"
                     },
                     data: [820, 932, 901, 934, 1290, 1330, 1320]
                 }
             ]
-        }
-       
-        return <EChartsReact
-        option={option}
-        style={{ height: 400 }}
-      />;
+        };
+
+        return <EChartsReact option={option} style={{ height: 400 }} />;
     }
 }
 
